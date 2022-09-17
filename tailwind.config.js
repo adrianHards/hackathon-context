25567917f7aea4d33e32de4eae16711d1c4d39ba
Rebecca Lim <rebeccalim23@gmail.com>
2022-09-17 15:42:04 +0100

    update tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,16 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    colors: {
+      'blue': '#38CFFF',
+      'light-blue': '#E5F9FF',
+      'teal': '#75FFE6',
+      'light-teal': '#DEFFF9',
+      'pink': '#FB3DBB',
+      'grey-font': '#ACACAC',
+      'orange': '#FFB800',
+    },
+  }
 }
