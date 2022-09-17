2e0fcefb70556b9ff3d640acda2ab3ab9215eb0f
Adrian <github@ahards.com>
2022-09-17 17:24:07 +0100

    fixed seed file by remove single quotes'

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ story = Story.new(
 )
 
 story.english =
-["When Daddy Rabbit suggests going to the skate park,", "Ruby’s big brother is excited,", "but little Ruby is nervous.", "What if she falls over and everyone laughs at her?",
-"The family arrive at the park and Ruby’s brother takes off.", "He zooms past Ruby,", "at turbo speed.", "That makes Ruby even more nervous.", "Daddy Rabbit gives Ruby a gentle nudge of encouragement.",
-"Daddy Rabbit says,", "'if you don’t try, you’ll never know'.", "Sensing Ruby’s uncertainty,", "Daddy Rabbit holds Ruby’s hand and they take the first step together.",
+["When Daddy Rabbit suggests going to the skate park,", "Ruby's big brother is excited,", "but little Ruby is nervous.", "What if she falls over and everyone laughs at her?",
+"The family arrive at the park and Ruby's brother takes off.", "He zooms past Ruby,", "at turbo speed.", "That makes Ruby even more nervous.", "Daddy Rabbit gives Ruby a gentle nudge of encouragement.",
+"Daddy Rabbit says,", "'if you don't try, you'll never know'.", "Sensing Ruby's uncertainty,", "Daddy Rabbit holds Ruby's hand and they take the first step together.",
 "Ruby slips a little but Daddy Rabbit catches her,", "and she tries again.", "Ruby is a natural!", "Ruby falls in love with skating and practices every day for months.",
-"She now can do tricks,", "like jumps and skating off rails!", "Her friends have nicknamed her ‘Ruby on Rails’."]
+"She now can do tricks,", "like jumps and skating off rails!", "Her friends have nicknamed her 'Ruby on Rails'."]
 
 story.spanish = {
   "When Daddy Rabbit suggests going to the skate park," => "Cuando papá Conejo sugiere ir al parque de patinaje,",
