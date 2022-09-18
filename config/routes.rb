03414e0e23e393644e4338e20af13240f407657e
Adrian <github@ahards.com>
2022-09-18 14:25:00 +0100

    quick fix for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "stories#home"
+
+  get "/next", to: "stories#next", as: :next
   resources :stories, only: [:home, :show, :index] do
     resources :sentences, only: [:show]
   end
