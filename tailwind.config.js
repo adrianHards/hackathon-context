329809d98f47dafd4e30625f914eb790e3a073e3
Rebecca Lim <rebeccalim23@gmail.com>
2022-09-18 12:13:07 +0100

    update homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
     // => @media (min-width: 640px) { ... }
     'laptop': '1024px',
     // => @media (min-width: 1024px) { ... }
-    'desktop': {'max': '1280px'},
+    'desktop': '1280px',
     // => @media (min-width: 1280px) { ... }
   }
 }
